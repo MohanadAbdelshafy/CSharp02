0506084a1a76a87d27f8a83311ecf6f555e62fee
mohandabdelshafy443-eng <mohandabdelshafy443@gmail.com>
2026-08-29 18:09:42 +0300

    Add project files.

---
+++ C#Basics-02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C_Basics_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
