908e4dcca8b24a60b2c011ba029d4dccdcd688df
mohandabdelshafy443-eng <mohandabdelshafy443@gmail.com>
2026-08-29 18:20:55 +0300

    Create a Book class with a Title (string) and Pages (int). Create a Book object and store it in a variable of type object. Print it.

--- C#Basics-02/Program.cs
+++ C#Basics-02/Program.cs
@@ -1,10 +1,21 @@
 ﻿namespace C_Basics_02
 {
+    #region BookClass
+    //public class Book
+    //{
+    //    public string Title;
+    //    public int Pages;
+    //}
+    #endregion
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Createing and printing Book Object
+            //Book b1 = new Book();
+            //object obj= b1;
+            //Console.WriteLine(obj);
+            #endregion
         }
     }
 }
